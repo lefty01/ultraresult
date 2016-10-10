bf0d68f829805dec653ffd02940118f3b40e6cdc
Andreas Loeffler <aloeff@de.ibm.com>
2016-10-10 14:32:51 +0200

    prepare for leaderboard

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ app.use('/', routes);
 
 app.use('/runners', runners);
 app.use('/aid', aidstation);
+app.use('/results', results);
+
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
