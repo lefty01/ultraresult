e2a0bbe8378e1bda2ebaaea6e0428f19864caad5
Andreas Loeffler <aloeff@de.ibm.com>
2017-12-19 20:17:12 +0100

    add table cells for pace/time/pause still wip

--- public/js/results.js
+++ public/js/results.js
@@ -65,11 +65,13 @@ function fillResultTable() {
 
 	    if ('START' === this.name) { return true; }
 	    if ('FINISH' === this.name) {
-		tableHeader += '<th>' + this.name + ' ' + this.directions + 
+		// colspan number of cells in finish coloumn: in, last pace, avg. pace, last time
+		tableHeader += '<th colspan="4">' + this.name + ' ' + this.directions + 
 		    ', @' + this.totalDistance + ',  &Delta; ' + this.legDistance + '</th>';
 		return true;
 	    }
-	    tableHeader += '<th colspan="2" id="' + this.name + '">' + this.name + ' '
+	    // for vp: colspan= in, out, pause, last pace, avg. pace, last time, total time
+	    tableHeader += '<th colspan="7" id="' + this.name + '">' + this.name + ' '
 		+ this.directions + ', @km ' + this.totalDistance + ',  &Delta; ' + this.legDistance + '</th>';
 	});
     });
@@ -81,8 +83,10 @@ function fillResultTable() {
 	    var outtime = "n/a";
 	    // todo ...
 	    var pause = "n/a";
-	    var pace = "n/a";
+	    var lastpace = "n/a";
 	    var avgpace = "n/a";
+	    var lasttime = "n/a";
+	    var totaltime = "n/a";
 	    var place = 'n/a';
 
 	    tableContent += '<tr>';
@@ -136,11 +140,19 @@ function fillResultTable() {
 		    return true;
 		}
 		if ("FINISH" === aidId) {
-		    tableContent += '<td>' + intime  + '</td>';
+		    tableContent += '<td>' + intime   + '</td>';
+		    tableContent += '<td>' + lastpace + '</td>';
+		    tableContent += '<td>' + avgpace  + '</td>';
+		    tableContent += '<td>' + lasttime + '</td>';
 		    return true;
 		}
-		tableContent += '<td>' + intime  + '</td>';
-		tableContent += '<td>' + outtime + '</td>';
+		tableContent += '<td>' + intime    + '</td>';
+		tableContent += '<td>' + outtime   + '</td>';
+		tableContent += '<td>' + pause     + '</td>';
+		tableContent += '<td>' + lastpace  + '</td>';
+		tableContent += '<td>' + avgpace   + '</td>';
+		tableContent += '<td>' + lasttime  + '</td>';
+		tableContent += '<td>' + totaltime + '</td>';
 	    });
 
 	    
@@ -167,17 +179,21 @@ function fillResultTable() {
 	tableHeader += '<th></th>';
 	tableHeader += '<th></th>';
 	$.each(aidStations, function() {
-		if ('START'  === this.name) { return true; }
-		if ('FINISH' === this.name) { return true; }
-		tableHeader += '<th>IN</th>';
-		tableHeader += '<th>OUT</th>';
-		// tableHeader += '<th class="" headers="' + this.name + '">T<sub>1</sub></th>';
-		// tableHeader += '<th class="" headers="' + this.name + '">T<sub>2</sub></th>';
-		// tableHeader += '<th class="" headers="' + this.name + '">P<sub>1</sub></th>';
-		// tableHeader += '<th class="" headers="' + this.name + '">P<sub>2</sub></th>';
+	    if ('START'  === this.name) { return true; }
+	    if ('FINISH' === this.name) { return true; }
+
+	    tableHeader += '<th>IN</th>';
+	    tableHeader += '<th>OUT</th>';
+	    tableHeader += '<th>Pause</th>';
+	    tableHeader += '<th>P<sub>1</sub></th>';
+	    tableHeader += '<th>P<sub>2</sub></th>';
+	    tableHeader += '<th>T<sub>1</sub></th>';
+	    tableHeader += '<th>T<sub>2</sub></th>';
 	});
-	tableHeader += '<th></th>';
-	tableHeader += '<th></th>';
+	tableHeader += '<th>IN</th>';
+	tableHeader += '<th>P<sub>1</sub></th>';
+	tableHeader += '<th>P<sub>2</sub></th>';
+	tableHeader += '<th>T<sub>1</sub></th>';
 	tableHeader += '<th></th>';
 	tableHeader += '</tr>';
 
@@ -188,83 +204,11 @@ function fillResultTable() {
 
 }
 
-function fillResultTableX(docTitle, thetime) {
-
-    var tableContent = '';
-    var tableHeader = '';
-    var runnerNum = 1;
-    var aidStations = [];
 
     // <caption>T<sub>1</sub>(hh:mm): Zeit zwischen VP<sub>n-1<sub>Tout</sub></sub> und  VP<sub>n<sub>Tin</sub></sub> , &nbsp;
     //      T<sub>2</sub>(hh:mm): Zeit zwischen Start und VP<sub>n<sub>Tin</sub></sub> , &nbsp;
     //      P<sub>1</sub>(mm:ss/km): &#216; Pace zwischen VP<sub>n-1<sub>Tout</sub></sub> und  VP<sub>n<sub>Tin</sub></sub> , &nbsp;
     // 	   P<sub>2</sub>(mm:ss/km): &#216; Pace zwischen Start und VP<sub>n<sub>Tin</sub></sub></caption>
-
-
-    tableHeader += '<tr>';
-    tableHeader += '<th class="sortable_numeric">#</th>';
-    tableHeader += '<th class="sortable_numeric">Rang</th>';
-    tableHeader += '<th class="name">Name</th>';
-    tableHeader += '<th class="starttime">Start</th>';
-
-    $.getJSON('/aid', function(data) {
-        $.each(data, function() {
-	    console.log("aid data: " + this.name);
-	    aidStations.push(this);
-
-	    if ('START' === this.name) { return true; }
-	    if ('FINISH' === this.name) {
-		tableHeader += '<th class="vp" colspan="5" id="' + this.name + '">' + this.name + ' '
-		    + this.directions + ', @km ' + this.totalDistance + ',  &Delta; ' + this.legDistance + '</th>';
-		return true;
-	    }
-	    tableHeader += '<th class="vp" colspan="7" id="' + this.name + '">' + this.name + ' '
-		+ this.directions + ', @km ' + this.totalDistance + ',  &Delta; ' + this.legDistance + '</th>';
-	});
-    });
-
-    tableHeader += '<th class="total">Zeit</th>';
-    tableHeader += '<th class="totalpause">&sum; Pause</th>';
-    tableHeader += '</tr>';
-
-    
-    $.getJSON('/runners', function(data) {
-        $.each(data, function() {
-	    var intime = "n/a";
-	    var outtime = "n/a";
-	    // todo ...
-	    var pause = "00";
-	    var pace = "6:00";
-	    var avgpace = "6:30";
-	    var place = 'n/a';
-
-
-            tableContent += '<td>' + this.startnum  + '</td>';
-	    tableContent += '<td>' + place + '</td>'; // place
-	    tableContent += '<td>' + this.firstname + ' ' + this.lastname + '</td>';
-
-
-	    // check the results field if we have valid times for this runner/aid
-	    var results = this.results;
-	    // for each aidstation ... aka $each(this.results, function() { ... })
-
-	    $.each(results, function(aidId, times) {
-		console.log(aidId + ": " + times);
-
-		if (results[aidId]) {
-		    console.log('fillStarterTable: ' + aidId + ' in valid:  ' + results[aidId].intime_valid);
-		    console.log('fillStarterTable: ' + aidId + ' in time:   ' + results[aidId].intime);
-		    console.log('fillStarterTable: ' + aidId + ' out valid: ' + results[aidId].outtime_valid);
-		    console.log('fillStarterTable: ' + aidId + ' out time:  ' + results[aidId].outtime);
-
-		    intime  = ("true" === results[aidId].intime_valid)  ? results[aidId].intime  : "n/a";
-		    outtime = ("true" === results[aidId].outtime_valid) ? results[aidId].outtime : "n/a";
-		}
-		// if ("Start" === aidId) {
-		//     tableContent += '<td>' + this.lastname  + '</td>';
-		// }
-		
-	    });
 
 	    
 
@@ -273,16 +217,6 @@ function fillResultTableX(docTitle, thetime) {
             // tableContent += '<td align="center">' + pause   + '</td>';
             // tableContent += '<td align="center">' + pace    + '</td>';
             // tableContent += '<td align="center">' + avgpace + '</td>';
-
-            tableContent += '</tr>';
-            runnerNum++;
-        });
-    });
-
-    // Inject the whole content string into our existing HTML table
-    $('#resultstable table thead').html(tableHeader);
-    $('#resultstable table tbody').html(tableContent);
-}
 
 
         // th(class="sorttable_numeric") Start #
@@ -295,18 +229,3 @@ function fillResultTableX(docTitle, thetime) {
         // th(class="sorttable_nosort") Pause (min)
         // th(class="sorttable_nosort") Pace
         // th(class="sorttable_nosort") Avg Pace
-   // <tr>
-   //  <th>#</th>
-
-   //  <th class="vp" colspan="7" id="vp1">VP1 Atdorf,            @mi 17.5,  Δ 17.5</th>
-   //  <th class="vp" colspan="7" id="vp2">VP2 Nufringen,         @mi 32.7,  Δ 15.22</th>
-   //  <th class="vp" colspan="7" id="vp3">VP3 Entringen,         @mi 46.7,  Δ 13.98</th>
-   //  <th class="vp" colspan="7" id="vp4">VP4 Tü Rittweg,   @mi 59.7,  Δ 12.99</th>
-   //  <th class="vp" colspan="7" id="vp5">VP5 Anders,            @mi 69.9,  Δ 10.25</th>
-   //  <th class="vp" colspan="7" id="vp6">VP6 Steinbrennerhaus,  @mi 83.5,  Δ 13.55</th>
-   //  <th class="vp" colspan="7" id="vp7">VP7 Teufelsbruch,      @mi 96.8,  Δ 13.36</th>
-   //  <th class="vp" colspan="5" id="vp8">Ziel Dettenhausen,     @mi 103.5, Δ 6.71</th>
-
-   //  <th class="total">Total</th>
-   //  <th>Urkunde</th>
-   // </tr></thead>
